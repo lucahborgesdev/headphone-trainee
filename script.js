cf86ff71dfa32b7a15f328457358d4424cf3ab33
Lucas Borges <lucashenrique251425@gmail.com>
2022-12-18 23:33:18 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function main1(){
+    var main1 = document.getElementById("main1").src="./img/product1.png"
+    var price01 = document.getElementById("price")
+    price01.innerHTML = "$89.00" 
+}
+
+function main2(){
+    var main2 = document.getElementById("main1").src="./img/product2.png"
+    var price02 = document.getElementById("price")
+    price02.innerHTML = "$129.00"
+}
+
+function main3(){
+    var main3 = document.getElementById("main1").src="./img/product3.png"
+    var price03 = document.getElementById("price")
+    price03.innerHTML = "$219.00"
+}
